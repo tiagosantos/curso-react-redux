fa98ca5b9336375d99df502e1d5bd1af14b63e3e
Carlos Tiago <ontiago@gmail.com>
2020-10-05 18:22:43 -0300

    adicionando JSX ao componente

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Primeiro from './components/basicos/Primeiro'
-const tag = <strong>Olá, ReactJS!</strong>
+
 ReactDOM.render(
   <div>
     <Primeiro></Primeiro>
-    { tag }
   </div>,
   document.getElementById('root')
 );
